a811337bc912167774eb76d2a1973ae6ef027164
Augusto Andrade <alias.tracking.na@gmail.com>
2026-02-21 12:14:42 -0400

    Add CRUD views with HTML templates for vehicles
    - Implement function-based views (FBV) for CRUD operations
    - Create templates: list, detail, form, delete
    - Add URLs routing for /veiculos/ endpoints
    - List: /veiculos/
    - Detail: /veiculos/<pk>/
    - Create: /veiculos/novo/
    - Update: /veiculos/<pk>/editar/
    - Delete: /veiculos/<pk>/excluir/

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Veiculo
 
-# Create your views here.
+
+def veiculo_list(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'vehicles/veiculo_list.html', {'veiculos': veiculos})
+
+
+def veiculo_detail(request, pk):
+    veiculo = get_object_or_404(Veiculo, pk=pk)
+    return render(request, 'vehicles/veiculo_detail.html', {'veiculo': veiculo})
+
+
+def veiculo_create(request):
+    if request.method == 'POST':
+        placa = request.POST['placa']
+        marca = request.POST['marca']
+        quantidade_passageiros = request.POST['quantidade_passageiros']
+        km_inicial = request.POST.get('km_inicial', 0)
+        kms_atual = request.POST.get('kms_atual', 0)
+        Veiculo.objects.create(
+            placa=placa,
+            marca=marca,
+            quantidade_passageiros=quantidade_passageiros,
+            km_inicial=km_inicial,
+            kms_atual=kms_atual
+        )
+        return redirect('veiculo_list')
+    return render(request, 'vehicles/veiculo_form.html', {})
+
+
+def veiculo_update(request, pk):
+    veiculo = get_object_or_404(Veiculo, pk=pk)
+    if request.method == 'POST':
+        veiculo.placa = request.POST['placa']
+        veiculo.marca = request.POST['marca']
+        veiculo.quantidade_passageiros = request.POST['quantidade_passageiros']
+        veiculo.km_inicial = request.POST.get('km_inicial', 0)
+        veiculo.kms_atual = request.POST.get('kms_atual', 0)
+        veiculo.save()
+        return redirect('veiculo_list')
+    return render(request, 'vehicles/veiculo_form.html', {'veiculo': veiculo})
+
+
+def veiculo_delete(request, pk):
+    veiculo = get_object_or_404(Veiculo, pk=pk)
+    if request.method == 'POST':
+        veiculo.delete()
+        return redirect('veiculo_list')
+    return render(request, 'vehicles/veiculo_confirm_delete.html', {'veiculo': veiculo})
